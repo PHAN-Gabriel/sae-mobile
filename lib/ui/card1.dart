d6b1b22fbfda437919b5273509c876e12359c631
melvyn <melvynpetit2003@gmail.com>
2023-04-07 20:29:19 +0200

    [ADD] affichage description complète par produit

--- lib/ui/card1.dart
+++ lib/ui/card1.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import '../api/my_api.dart';
 
+import '../detail.dart';
 import '../models/article.dart';
 import '../models/todo.dart';
 
@@ -12,60 +13,70 @@ class Ecran1 extends StatefulWidget{
 
 class _Ecran1State extends State<Ecran1> {
   late Future<List<Article>> futureTodo;
-  MyAPI myAPI =MyAPI();
+  MyAPI myAPI = MyAPI();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     futureTodo = myAPI.getArticles();
   }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: futureTodo,
-        builder: (context,snapshot){
-          if (snapshot.hasData){
-            return GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // nombre de colonnes
+      future: futureTodo,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // nombre de colonnes
+            ),
+            itemCount: snapshot.data?.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => Detail(article: snapshot.data![index]),
+                  ));
+                },
+                child: Card(
+                  elevation: 6,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Image.network(
+                          snapshot.data?[index].image ?? "",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          snapshot.data?[index].title ?? "",
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "${snapshot.data?[index].price.toString()} €" ?? "",
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                itemCount: snapshot.data?.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 6,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: Image.network(
-                            snapshot.data?[index].image ?? "",
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            snapshot.data?[index].title ?? "",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            "${snapshot.data?[index].price.toString()} €" ?? "",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                });
-          }
-          else if (snapshot.hasError){
-            return Text('${snapshot.error}');
-          }
-          else{
-            return const Center(child: CircularProgressIndicator(),);
-          }
-        });
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
